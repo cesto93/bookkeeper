10fc65a473f03aaca5ecac4835a79d22e7db4a0a
cesto93 <cesto93@hotmail.it>
2020-06-15 17:40:09 +0200

    added second test

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/CookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/CookieTest.java
@@ -1,5 +1,6 @@
 package org.apache.bookkeeper.bookie;
 
+import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,8 +15,31 @@ public class CookieTest {
             Cookie.encodeDirPaths(null);
             fail("Not raising exception");
         } catch (Exception e) {
-
+            System.out.println("Expected exception " + e.getMessage());
         }
         assertEquals("0", Cookie.encodeDirPaths(new String[0]));
+    }
+
+    @Test
+    public void testGetLedgerDirPathsFromCookie() {
+        Cookie.Builder cookieBuilder = Cookie.newBuilder();
+        cookieBuilder.setLedgerDirs("2\tdir1\tdir2");
+        Cookie cookie1 = cookieBuilder.build();
+        String[] dirs = cookie1.getLedgerDirPathsFromCookie();
+        assertArrayEquals(new String[] {"dir1", "dir2"} , dirs);
+
+        try {
+            cookieBuilder = Cookie.newBuilder();
+            Cookie cookie2 = cookieBuilder.build();
+            cookie2.getLedgerDirPathsFromCookie();
+            fail("Not raising exception");
+        } catch (Exception e) {
+            System.out.println("Expected exception " + e.getMessage());
+        }
+
+        cookieBuilder.setLedgerDirs("");
+        Cookie cookie3 = cookieBuilder.build();
+        dirs = cookie3.getLedgerDirPathsFromCookie();
+        assertEquals("", dirs[0]);
     }
 }
